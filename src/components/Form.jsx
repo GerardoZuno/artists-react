d218a8090df4a929dd59b62ad791e62b052c7993
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-23 20:16:55 -0500

    Add Form Component

---
+++ src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="bg-info">
+      <div className="container">
+          <form>
+            <div className="col card text-white bg-transparent mb-5 pt-5 pb-2"></div>
+            <fieldset>
+              <legend className="text-center">Buscador Letras Canciones</legend>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artista</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="artista"
+                      placeholder="Nombre Artista"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Cancion</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="cancion"
+                      placeholder="Nombre Cancion"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="btn  btn-primary float-right"
+              >
+                Buscar
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    
+  );
+}
+
+export default Form;
